ab31becde2bbac3b85eae08a2193f0bb7accd592
unknown <jimmyale3102@gmail.com>
2022-08-23 17:35:30 -0500

    Add textToSpeach

--- app/src/main/java/dev/alejo/texttospeach/MainActivity.kt
+++ app/src/main/java/dev/alejo/texttospeach/MainActivity.kt
@@ -2,10 +2,34 @@ package dev.alejo.texttospeach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import dev.alejo.texttospeach.databinding.ActivityMainBinding
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initComponents()
     }
+
+    private fun initComponents() {
+        tts = TextToSpeech(this, this)
+        binding.btnPlay.setOnClickListener { speak() }
+    }
+
+    private fun speak() {
+        tts!!.speak(binding.message.text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS)
+            tts!!.language = Locale.US
+    }
+
 }
